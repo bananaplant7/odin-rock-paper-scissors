09a685316081c5c81d7e86d59e20b3021c67b420
bananaplant7 <brooklyncheng2002@gmail.com>
2022-03-08 18:23:16 -0800

    change round results to display in div (instead of console), add running score

--- file.js
+++ file.js
@@ -4,8 +4,9 @@ let myArray = [
     'scissors'
 ];
 
-function computerPlay() { 
-    return myArray[Math.floor(Math.random()*myArray.length)];
+// random selection between r,p,s
+function computerPlay() {
+    return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -13,85 +14,92 @@ function playRound(playerSelection, computerSelection) {
         playerSelection.toLowerCase() === 'rock'
         &&
         computerSelection.toLowerCase() === 'rock'
-        ) {
-            return "Rock vs Rock is a tie"
-        } else if
+    ) {
+        return "Rock vs Rock is a tie";
+    } else if
         (
         playerSelection.toLowerCase() === 'rock'
         &&
         computerSelection.toLowerCase() === 'paper'
-        ) {
-            return "You lose! Paper beats Rock"
-        } else if
+    ) {
+        return "You lose! Paper beats Rock";
+    } else if
         (
         playerSelection.toLowerCase() === 'rock'
         &&
         computerSelection.toLowerCase() === 'scissors'
-        ) {
-            return "You win! Rock beats Scissors"
-        } else if
+    ) {
+        return "You win! Rock beats Scissors";
+    } else if
         (
         playerSelection.toLowerCase() === 'paper'
         &&
         computerSelection.toLowerCase() === 'rock'
-        ) {
-            return "You win! Paper beats Rock"
-        } else if
+    ) {
+        return "You win! Paper beats Rock";
+    } else if
         (
         playerSelection.toLowerCase() === 'paper'
         &&
         computerSelection.toLowerCase() === 'paper'
-        ) {
-            return "Paper vs Paper is a tie"
-        } else if
+    ) {
+        return "Paper vs Paper is a tie";
+    } else if
         (
         playerSelection.toLowerCase() === 'paper'
         &&
         computerSelection.toLowerCase() === 'scissors'
-        ) {
-            return "You lose! Scissors beats Paper"
-        } else if
+    ) {
+        return "You lose! Scissors beats Paper";
+    } else if
         (
         playerSelection.toLowerCase() === 'scissors'
         &&
         computerSelection.toLowerCase() === 'rock'
-        ) {
-            return "You lose! Rock beats Scissors"
-        } else if
+    ) {
+        return "You lose! Rock beats Scissors";
+    } else if
         (
         playerSelection.toLowerCase() === 'scissors'
         &&
         computerSelection.toLowerCase() === 'paper'
-        ) {
-            return "You win! Scissors beats Paper"
-        } else 
-            return "Scissors vs Scissors is a tie"  
+    ) {
+        return "You win! Scissors beats Paper";
+    } else
+        return "Scissors vs Scissors is a tie";
 }
 
-const buttons = document.querySelectorAll('button')
+let playerScore = 0;
+let computerScore = 0;
+
+const results = document.querySelector('.results');
+
+const score = document.querySelector('.score');
+score.textContent = `${playerScore}-${computerScore}`;
+
+const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', function () {
-    console.log(playRound(button.className, computerPlay()))}))
+    results.textContent = playRound(button.className, computerPlay());
+    keepScore();
+}));
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    let a = playRound(prompt("choose"), computerPlay())
-    console.log(a)
-
-    if (a.includes("win")) {
-        console.log(`${playerScore += 1}-${computerScore}`);
-    } else if (a.includes("lose")) {
-        console.log(`${playerScore}-${computerScore += 1}`);
-    } else {
-        console.log(`${playerScore}-${computerScore}`);
+// ends game when a score = 5
+function winOrLose() {
+    if (playerScore == 5) {
+        results.textContent = ("you won! :D")
+    } else if (computerScore == 5) {
+        results.textContent = ("you louse + garbage + balding + L + ratio")
     }
-    
-    // if (playerScore < computerScore) {
-    //     console.log("You lose");
-    // } else if (playerScore > computerScore) {
-    //     console.log("You win");
-    // } else {
-    //     console.log("It's a tie");
-    // } 
+}
+
+function keepScore() {
+    if (results.textContent.includes("win")) {
+        score.textContent = (`${playerScore += 1}-${computerScore}`);
+        winOrLose()
+    } else if (results.textContent.includes("lose")) {
+        score.textContent = (`${playerScore}-${computerScore += 1}`);
+        winOrLose()
+    } else {
+        score.textContent = (`${playerScore}-${computerScore}`);
+    } 
 }
